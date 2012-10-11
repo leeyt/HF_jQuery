06f6d174e935aea8c58f6b52741090db617dd37a
neillee <neillee@potix.com>
2012-10-11 22:32:38 +0800

    Chapter 03: get discount only if guessed correctly

--- ch03/scripts/my_scripts.js
+++ ch03/scripts/my_scripts.js
@@ -1,12 +1,37 @@
 $(document).ready(function() {
   $(".guess_box").click(checkForCode);
-  var checkForCode = function() {
-    var my_num = Math.floor((Math.random()*5) + 5);
-    var discount = "<p>Your Discount is " + my_num + "%</p>";
+
+  function getRandom(num) {
+    var my_num = Math.floor(Math.random()*num);
+    return my_num;
+  }
+
+  var hideCode = function() {
+    var numRand = getRandom(4);
+    $(".guess_box").each(function(index, value) {
+      if (numRand == index) {
+        $(this).append("<span id='has_discount'></span>");
+        return false;
+      }
+    });
+  }
+
+  hideCode();
+
+  function checkForCode() {
+    var discount;
+
+    if ($.contains(this, document.getElementById("has_discount"))) {
+      var my_num = getRandom(5);
+      discount = "<p>Your Discount is " + my_num + "%</p>";
+    } else {
+      discount = "<p>Sorry, no discount this time!</p>";
+    }
     $(this).append(discount);
 
     $(".guess_box").each(function() {
       $(this).unbind("click");
     });
-  };
+  }
 });
+
