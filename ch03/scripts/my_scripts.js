4be2bb0141781f11510798d01074238228f5b433
neillee <neillee@potix.com>
2012-10-11 22:00:45 +0800

    Chapter 03: give handler function a name

--- ch03/scripts/my_scripts.js
+++ ch03/scripts/my_scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $(".guess_box").click( function() {
+  $(".guess_box").click(checkForCode);
+  var checkForCode = function() {
     var my_num = Math.floor((Math.random()*5) + 5);
     var discount = "<p>Your Discount is " + my_num + "%</p>";
     $(this).append(discount);
@@ -7,5 +8,5 @@ $(document).ready(function() {
     $(".guess_box").each(function() {
       $(this).unbind("click");
     });
-  });
+  };
 });
