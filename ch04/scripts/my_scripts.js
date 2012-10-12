75a4706c86123b256d50d3822a4e278edae32e3b
neillee <neillee@potix.com>
2012-10-12 21:48:14 +0800

    Chapter 04: add buttons to switch menu display state

--- ch04/scripts/my_scripts.js
+++ ch04/scripts/my_scripts.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+	var v = false;
 
-	
+	$("#vegOn").click(function() {
+		if (v == false) {
+			v = true;
+		}
+	});
+
+	$("#restoreMe").click(function() {
+		if (v == true) {
+			v = false;
+		}
+	});
 });
