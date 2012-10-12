c401fa64d845af9297acb421064f9962cc32b731
neillee <neillee@potix.com>
2012-10-12 16:21:51 +0800

    Chapter 03: hide discount code in one of the box and reveal where the code is hiding after guess is made

--- ch03/scripts/my_scripts.js
+++ ch03/scripts/my_scripts.js
@@ -32,16 +32,22 @@ $(document).ready(function() {
     var discount;
 
     if ($.contains(this, document.getElementById("has_discount"))) {
-      var my_num = getRandom(5);
-      discount = "<p>Your Discount is " + my_num + "%</p>";
+      var my_num = getRandom(100);
+      discount = "<p>Your Code: CODE" + my_num + "</p>";
     } else {
       discount = "<p>Sorry, no discount this time!</p>";
     }
-    $(this).append(discount);
 
     $(".guess_box").each(function() {
-      $(this).unbind("click");
+      if ($.contains(this, document.getElementById("has_discount"))) {
+        $(this).addClass("discount");
+      } else {
+        $(this).addClass("no_discount");
+      }
+      $(this).unbind();
     });
+
+    $("#result").append(discount);
   }
 });
 
