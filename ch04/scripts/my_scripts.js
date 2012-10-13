a30a01450aea463f8ae530bac87f43d38cb2c23a
neillee <neillee@potix.com>
2012-10-13 17:40:50 +0800

    Chapter 04: finish the menu script

--- ch04/scripts/my_scripts.js
+++ ch04/scripts/my_scripts.js
@@ -4,12 +4,26 @@ $(document).ready(function(){
 	$("#vegOn").click(function() {
 		if (v == false) {
 			v = true;
+
+			$f = $("li.fish").parent().parent().detach();
+			$(".hamburger").replaceWith("<li class='portobello'>Portobello Mushroom</li>");
+			$(".meat").after("<li class='tofu'><em>Tofu</em></li>");
+			$m = $(".meat").detach();
+			$(".tofu").parent().parent().addClass("veg_leaf");
 		}
+
 	});
 
 	$("#restoreMe").click(function() {
 		if (v == true) {
 			v = false;
+
+			$(".menu_entrees li").first().before($f);
+			$(".portobello").replaceWith("<li class='hamburger'>hamburger</li>");
+			$(".tofu").each(function(i) {
+				$(this).parent().parent().removeClass("veg_leaf");
+				$(this).after($m[i]).remove();
+			});
 		}
 	});
 });
